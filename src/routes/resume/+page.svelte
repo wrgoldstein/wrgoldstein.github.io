32ee04424e37b1f7823ff1d8dd876e8fa25ff697
Will <williamrgoldstein@gmail.com>
2024-11-13 10:42:42 -0500

    Add resume skeleton

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -1,3 +1,45 @@
+<script>
+  const projects = [
+    {
+      company: "artsy",
+      type: "data engineering",
+      short: "Set up data infrastructure with Segment, Redshift, and Looker",
+    },
+    {
+      company: "artsy",
+      type: "software engineering",
+      short: "Filtered search with Elasticsearch",
+    },
+    {
+      company: "artsy",
+      type: "management",
+      short: "Managed four analysts",
+    },
+    {
+      company: "artsy",
+      type: "machine learning",
+      short: "Saved 50 hours/week with ML / automation",
+    },
+    {
+      company: "howaboutwe",
+      type: "data science",
+      short: "built churn prediction models"
+    },
+    {
+      company: "better",
+      type: "design",
+      short: "Built Jupyter-based knowledge sharing platform",
+    },
+    {
+      company: "better",
+      type: "data engineering",
+      short: "Set up Airflow",
+    },
+    
+  ]
+</script>
+
+
 <div class="mt-12 space-y-8">
   <a class="underline hover:font-bold" href="/">back</a>
   <div>data engineering</div>
